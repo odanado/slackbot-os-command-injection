b1f774a339cba5b5976833c8104c0c99d873beee
odan <odan3240@gmail.com>
2016-10-25 00:22:37 +0900

    add StartArgs

--- constant.py
+++ constant.py
@@ -7,5 +7,13 @@ Command['bash']['run'] = "bash a.out"
 
 DockerImage = 'odanado/os-command-injection'
 
+StartArgs = {}
+StartArgs['--net'] = 'none'
+StartArgs['--cpuset-cpus'] = '0'
+StartArgs['--memory'] = '512m'
+StartArgs['--memory-swap'] = '512m'
+StartArgs['-w'] = '/workspace'
+StartArgs['--ulimit'] = ['nproc=10:10', 'fsize=1000000']
+
 if __name__ == '__main__':
     print(Command)
