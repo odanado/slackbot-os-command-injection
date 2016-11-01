340bbde9dca11006266f9633014458637e58862a
odan <odan3240@gmail.com>
2016-11-01 17:14:57 +0900

    add c-lang command

--- constant.py
+++ constant.py
@@ -5,6 +5,11 @@ Command['bash']['filename'] = "Main.sh"
 Command['bash']['compile'] = "cat Main.sh | tr -d '\\r' >a.out"
 Command['bash']['run'] = "bash a.out"
 
+Command['c'] = {}
+Command['c']['filename'] = "Main.c"
+Command['c']['compile'] = "gcc -O2 -o ./a.out ./Main.c -lm"
+Command['c']['run'] = "./a.out"
+
 DockerImage = 'odanado/os-command-injection'
 
 StartArgs = {}
