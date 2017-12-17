266f91b5659d069bc3d4f7654dfb6ad25902ed73
odanado <odanado@users.noreply.github.com>
2017-12-17 20:39:52 +0900

    Add brainfuck (#31)
    * add brainfuck
    
    * add test of brainfuck

--- tests/test_code_runner.py
+++ tests/test_code_runner.py
@@ -32,3 +32,18 @@ print('stderr', file=sys.stderr)
             self.code_runner.run(self.source_code, **self.config)
         self.assertEqual(stdout, 'Hello World!')
         self.assertEqual(stderr, 'stderr')
+
+
+class TestBrainfuckRunner(unittest.TestCase):
+    def setUp(self):
+        self.code_runner = CodeRunner('odanado/os-command-injection')
+        self.source_code = """
++++++++++[>++++++++>+++++++++++>+++++<<<-]>.>++.+++++++..+++.>-.
+------------.<++++++++.--------.+++.------.--------.>+.
+"""
+        self.config = yaml.load(open('./langs/brainfuck/config.yml'))
+
+    def test_run(self):
+        stdout, _, _ = \
+            self.code_runner.run(self.source_code, **self.config)
+        self.assertEqual(stdout, 'Hello, world!')
