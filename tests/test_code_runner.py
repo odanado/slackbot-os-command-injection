2f70d3f96c4771fcfab7e6187c95a40b85459487
odan <odan3240@gmail.com>
2018-03-17 00:53:47 +0900

    add dynamic unittest

--- tests/test_code_runner.py
+++ tests/test_code_runner.py
@@ -1,6 +1,7 @@
 import sys
 import unittest
 import yaml
+from pathlib import Path
 
 sys.path.append('./plugins')
 from code_runner import CodeRunner  # NOQA
@@ -69,3 +70,45 @@ Console.WriteLine("Hello, world!");
         stdout, _, _ = \
             self.code_runner.run(self.source_code, **self.config)
         self.assertEqual(stdout, 'Hello, world!')
+
+
+class TestCodeRunner(unittest.TestCase):
+    def setUp(self):
+        self.code_runner = CodeRunner('odanado/os-command-injection')
+
+
+def make_test(test_file, config):
+    def test(self):
+        stdout_file = test_file.with_suffix('.stdout')
+        stderr_file = test_file.with_suffix('.stderr')
+
+        source_code = test_file.read_text()
+
+        stdout, stderr, _ = self.code_runner.run(source_code, **config)
+
+        self.assertTrue(stdout_file.exists())
+
+        self.assertEqual(stdout, stdout_file.read_text().strip())
+
+        if stderr_file.exists():
+            self.assertEqual(stderr, stderr.read_text().strip())
+
+    return test
+
+
+def register_tests(path):
+    path = Path(path)
+    tests_path = path / 'tests'
+    config = yaml.load((path / 'config.yml').read_text())
+    suffix = Path(config['filename']).suffix
+
+    test_files = tests_path.glob('*' + suffix)
+
+    tests = []
+    for test_file in test_files:
+        setattr(TestCodeRunner, test_file.name, make_test(test_file, config))
+
+    return tests
+
+
+register_tests('./langs/bash')
