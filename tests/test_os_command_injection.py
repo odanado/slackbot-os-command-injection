828aef0ae2ddacb7c36d7a4303d42033e4517dc2
odan <odan3240@gmail.com>
2018-06-06 14:46:36 +0000

    add test

--- tests/test_os_command_injection.py
+++ tests/test_os_command_injection.py
@@ -2,7 +2,7 @@ import sys
 import unittest
 
 sys.path.append('./plugins')
-from os_command_injection import get_config  # NOQA
+from os_command_injection import get_config, get_lang  # NOQA
 
 
 class TestOSCommandInjection(unittest.TestCase):
@@ -12,3 +12,8 @@ class TestOSCommandInjection(unittest.TestCase):
     def test_get_config(self):
         config = get_config('c')
         self.assertIsNotNone(config)
+
+    def test_get_lang(self):
+        self.assertEqual('bash', get_lang('mention'))
+        self.assertEqual('c', get_lang('mention\nc'))
+        self.assertEqual('java', get_lang('mention\nJava'))
